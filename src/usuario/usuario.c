757e765c4373c880df414b146e23841e1cd7c477
gabriel <gabriel@gbl.velasquez>
2025-06-15 23:41:56 -0300

    minhas alterações no cadastro de usuário e outras funcionalidades

--- src/usuario/usuario.c
+++ src/usuario/usuario.c
@@ -3,4 +3,18 @@
 #include "usuario.h"
 #include <stdio.h>
 
-//void cadastrar_usuario(FILE *arq_usuarios) {}
+void cadastrar_usuario(FILE *arq_usuarios) {
+    usuario NovoUser;
+    
+    printf("Codigo: ");
+    scanf("%d", NovoUser.codigo);
+    printf("\nNome: ");
+    scanf("%[^\n]%*c ",NovoUser.nome);
+
+    inserir_Usuario_Cabeca(arq_usuarios,NovoUser);
+}
+
+void inserir_Usuario_Cabeca(FILE *arq_usuarios, usuario UserParaInserir){
+    cabecalho *cab = le_cabecalho(arq_usuarios);
+    
+}
